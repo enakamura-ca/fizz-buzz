c37c8c3f71ede4d2ba7853e47b6234b726f9cfef
Edgar <57776301+enakamura-ca@users.noreply.github.com>
2024-02-05 17:08:16 +0000

    Fixed 3 equals sintax error

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 export default function fizzBuzz(input) {
-  if (input % 3 === 0 && input % 5 === 0) {
+  if (input % 3 == 0 && input % 5 == 0) {
     return "FizzBuzz";
-  } else if (input % 3 === 0) {
+  } else if (input % 3 == 0) {
     return "Fizz";
-  } else if (input % 5 === 0) {
+  } else if (input % 5 == 0) {
     return "Buzz";
   } else {
     return input;
